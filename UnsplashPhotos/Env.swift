11245f8c8456ab50e5f9d5c21bb5a91ae58355ba
SengIlWoo <startolove11@naver.com>
2020-11-19 00:50:12 +0900

    Unsplash Key added

---
+++ UnsplashPhotos/Env.swift
@@ -0,0 +1,13 @@
+//
+//  Env.swift
+//  UnsplashPhotos
+//
+//  Created by ilwoo sung on 2020/11/19.
+//
+
+import Foundation
+
+enum Unsplash {
+    static let accessKey: String = "NGrF6Oqzbz-TOnUjKNHMoy3KKMpjhS-MwAHsPZhfdD0"
+    static let secretKey: String = "NmDZfyWIjK8hkXa8N2tYgS8jJPA5G2TsGf-_4-nYkX8"
+}
