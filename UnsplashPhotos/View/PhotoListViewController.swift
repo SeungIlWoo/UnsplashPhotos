fa316f1f542d52e67ae3084148537a82799951d3
SengIlWoo <startolove11@naver.com>
2020-11-19 01:14:34 +0900

    ViewController, ViewModel configure

--- UnsplashPhotos/View/PhotoListViewController.swift
+++ UnsplashPhotos/View/PhotoListViewController.swift
@@ -16,4 +16,40 @@ final class PhotoListViewController: UITableViewController {
     let viewModel = ViewModel()
     let disposeBag = DisposeBag()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = nil
+        tableView.dataSource = nil
+
+        tableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
+
+        viewModel.photos
+            .bind(to: tableView.rx.items(cellIdentifier: "PhotoCell",
+                                         cellType: PhotoCell.self)) { (row, element, cell) in
+
+                cell.configure(model: element)
+
+                cell.downloadButton.rx.tap
+                    .throttle(.seconds(1), scheduler: MainScheduler.instance)
+                    .subscribe(onNext: { [unowned self] photo in
+                        self.viewModel.savePhoto(element)
+                    })
+                .disposed(by: self.disposeBag)
+            }
+            .disposed(by: disposeBag)
+
+        viewModel.fetch()
+
+    }
+
+    // MARK: - UIScrollViewDelegate
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let distance = scrollView.contentSize.height - (targetContentOffset.pointee.y + scrollView.bounds.height)
+
+        if viewModel.loadingState == .loaded && distance < 400 {
+            viewModel.fetch()
+        }
+    }
 }
--- UnsplashPhotos/ViewModel/PhotosViewModel.swift
+++ UnsplashPhotos/ViewModel/PhotosViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import os
 
 protocol PhotosViewModelType {
 
@@ -22,9 +23,65 @@ protocol PhotosViewModelType {
 
 class PhotosViewModel: PhotosViewModelType {
 
-    var photos = BehaviorRelay<[Photo]>(value: [])
-    var page: Int = 1
-    func fetch() {
-
+    // 로딩 상태 값
+    enum LoadingState {
+        case loading
+        case loaded
     }
+
+    var photos = BehaviorRelay<[Photo]>(value: [])
+    let service = UnsplashService()
+    var page: Int = 1
+    let disposeBag = DisposeBag()
+
+    var loadingState: LoadingState = .loaded
+
+    // 가져오기
+    func fetch() {
+        self.loadingState = .loading
+        try? service.fetch(parameter: PhotosParameterModel(page: page))
+            .subscribe(onNext: { [weak self] (response, result) in
+                guard let `self` = self else { return }
+
+                os_log("response: \(response)")
+                os_log("result: \(result as! NSObject)")
+
+                var photos: [Photo]
+                if self.page == 1 {
+                    photos = self.getPhotos(object: result)
+                } else {
+                    photos = self.photos.value + self.getPhotos(object: result)
+                }
+                self.photos.accept(photos)
+                self.page = self.page + 1
+                self.loadingState = .loaded
+            }).disposed(by: disposeBag)
+    }
+
+    func getPhotos(object: Any) -> [Photo] {
+
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else { return [] }
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        decoder.dataDecodingStrategy = .deferredToData
+        let photos: [Photo] = try! decoder.decode(Array<Photo>.self, from: data)
+
+        return photos
+    }
+
+    func savePhoto(_ photo: Photo) {
+        print("photo: \(photo.id)")
+
+        if let url = URL(string: photo.urls.thumb) {
+            print("url: \(url)")
+            if let data = try? Data(contentsOf: url) {
+                print("data")
+                if let image = UIImage(data: data) {
+                    print("image")
+                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                }
+            }
+        }
+    }
+
 }
